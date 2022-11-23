d4fc5fea3f71b3f24f83a1ddb8defcb317bf3723
codebyasif/Protfolio <codebyasif@gmail.com>
2022-11-23 14:05:58 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+     effect: 'cube',
+      grabCursor: true,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+     },
+     autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+     },
+     speed: 1000,
+    
+     pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+     },
+    
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+
+
